d321ebf862f8d5db6dea2696118911c39f49a48e
Raikoff <podchernin.ivan@gmail.com>
2022-06-21 11:49:29 +0300

    update v0.1

---
+++ Input.java
@@ -0,0 +1,22 @@
+
+public class Input {
+    public double n1;
+    public double n2;
+    public String action;
+
+    public Input(Double n1, Double n2, String action) {  // конструктор значений
+
+        this.n1 = n1;
+
+        this.n2 = n2;
+
+        this.action = action;
+    }
+
+    // public String getInfo() {
+    //     return String.format("%f %f %s", this.n1, this.n2, this.action);
+
+    // }
+
+    
+}
