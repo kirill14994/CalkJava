3dea197fa72dd7befca279c5e85ade58e1698f8f
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2022-06-23 15:49:11 +0200

    Fix

--- Controller.java
+++ Controller.java
@@ -4,22 +4,20 @@ public class Controller {
     public void start() {
 
         Scanner scan = new Scanner(System.in);
+        Input inpt = new Input();
+        Logic calc = new Logic();
+        
         System.out.println("Выберите: ");
         System.out.println("1 - Коплексные числа;");
         System.out.println("2 - Рациональные;");
         int choise = scan.nextInt();
 
-        if (choise == 1) {
-            Input inpt = new Input();
 
-            Logic calc = new Logic();
+        if (choise == 1) {
             calc.logicSec(inpt.inputCompelx());
         }
 
         else if (choise == 2) {
-            Input inpt = new Input();
-            Logic calc = new Logic();
-
             calc.logicRac(inpt.inputRacio());
         }
 
